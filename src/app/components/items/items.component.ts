666e290dcd63e8641536d656d46cea8b35865be5
S <aa>
2023-07-07 08:33:13 -0300

    Segundo intento de repositorio

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+  
+  items: Item[] = [];
+
+  constructor () {}
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: "manzana",
+        price: 10.5,
+        quantity: 4,
+        completed: false,
+      },
+
+      {
+        id: 1,
+        title: "pan",
+        price: 3.5,
+        quantity: 4,
+        completed: true,
+      }
+    ];
+  }
+
+}
